8de732333f35b7ebab340a9edc2f43ded6bf29ca
cristhian <152760161+pcristhian@users.noreply.github.com>
2025-05-03 22:19:38 +0000

    primer cliente

--- app/data/personas.js
+++ app/data/personas.js
@@ -1,7 +1,7 @@
 // /app/data/personas.js
 
 const personas = [
-  { nombre: "Juan", apellido: "Pérez", codigo: "A001", puntos: 120, imagen: "https://res.cloudinary.com/dlll6ncvv/image/upload/v1746279227/cliente_fd14ci.png" },
+  { nombre: "Elva", apellido: "Choque", codigo: "D-1 V5-032", puntos: 180, imagen: "https://res.cloudinary.com/dlll6ncvv/image/upload/v1746279227/cliente_fd14ci.png" },
   { nombre: "Ana", apellido: "Gómez", codigo: "A002", puntos: 95, imagen: "https://res.cloudinary.com/dlll6ncvv/image/upload/v1746279227/cliente_fd14ci.png" },
   { nombre: "Luis", apellido: "Ramírez", codigo: "A003", puntos: 150, imagen: "https://res.cloudinary.com/dlll6ncvv/image/upload/v1746279227/cliente_fd14ci.png" },
   { nombre: "Carla", apellido: "Fernández", codigo: "A004", puntos: 80, imagen: "https://res.cloudinary.com/dlll6ncvv/image/upload/v1746279227/cliente_fd14ci.png" },
