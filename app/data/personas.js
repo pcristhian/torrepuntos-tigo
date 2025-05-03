8d931c27b3526095859a95dbf14bd8ff3771fca4
cristhian <152760161+pcristhian@users.noreply.github.com>
2025-05-03 23:00:00 +0000

    clientes

--- app/data/personas.js
+++ app/data/personas.js
@@ -2,8 +2,8 @@
 
 const personas = [
   { nombre: "Elva", apellido: "Choque", codigo: "D-1 V5-032", puntos: 180, imagen: "https://res.cloudinary.com/dlll6ncvv/image/upload/v1746312311/elva_choque_qhixl2.jpg" },
-  { nombre: "Ana", apellido: "Gómez", codigo: "A002", puntos: 95, imagen: "https://res.cloudinary.com/dlll6ncvv/image/upload/v1746279227/cliente_fd14ci.png" },
-  { nombre: "Luis", apellido: "Ramírez", codigo: "A003", puntos: 150, imagen: "https://res.cloudinary.com/dlll6ncvv/image/upload/v1746279227/cliente_fd14ci.png" },
+  { nombre: "Julia", apellido: "Paco Tancara", codigo: "C5-021", puntos: 148, imagen: "https://res.cloudinary.com/dlll6ncvv/image/upload/v1746279227/cliente_fd14ci.png" },
+  { nombre: "Dionio", apellido: "Ticona", codigo: "D3-C-0005", puntos: 1430, imagen: "https://res.cloudinary.com/dlll6ncvv/image/upload/v1746279227/cliente_fd14ci.png" },
   { nombre: "Carla", apellido: "Fernández", codigo: "A004", puntos: 80, imagen: "https://res.cloudinary.com/dlll6ncvv/image/upload/v1746279227/cliente_fd14ci.png" },
   { nombre: "Santiago", apellido: "Muñoz", codigo: "A048", puntos: 190, imagen: "https://res.cloudinary.com/dlll6ncvv/image/upload/v1746279227/cliente_fd14ci.png" },
   { nombre: "Teresa", apellido: "Guzmán", codigo: "A049", puntos: 225, imagen: "https://res.cloudinary.com/dlll6ncvv/image/upload/v1746279227/cliente_fd14ci.png" },
